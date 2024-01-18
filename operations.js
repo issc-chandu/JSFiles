693da6e93f2e83a65bab1a60bc2f569a1b47f37f
issc-chandu <patilchandrashekhar1023@gmail.com>
2024-01-18 18:31:51 +0530

    op

---
+++ operations.js
@@ -0,0 +1,48 @@
+let someNumber = 11;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//********************Operations********************
+
+let value = 10;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3)
+console.log(2/3);
+console.log(2%3);
+
+let str1 ="Hello";
+let str2 = " Hitesh";
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3); //Please the look like this type to write code 
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
