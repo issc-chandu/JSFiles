1371782f1cf844567357d23894032eb70b73bea7
issc-chandu <patilchandrashekhar1023@gmail.com>
2024-01-18 16:00:34 +0530

    change

---
+++ datatype.js
@@ -0,0 +1,17 @@
+// let age = "33";
+
+// console.log(typeof age);
+
+// let valueInNumber = Number(age);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+stringNumber = String(someNumber);
+console.log(typeof stringNumber);
